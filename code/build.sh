a7d1048fbe83d8f4626ac212d310d6b9a2478d8a
rivten <hugo.viala@gmail.com>
2017-06-05 00:04:10 +0200

    switch from SDL_Net to SDL2_Net

--- code/build.sh
+++ code/build.sh
@@ -1,3 +1,3 @@
 ~/dev/ctime/ctime -begin compil_timings.ctm
-g++ -g -std=c++11 -Wno-write-strings -I ../../rivten/ gardakan.cpp -l SDL2 -l SDL_net -o ../build/gardakan.sh
+g++ -g -std=c++11 -Wno-write-strings -I ../../rivten/ gardakan.cpp -l SDL2 -l SDL2_net -o ../build/gardakan.sh
 ~/dev/ctime/ctime -end compil_timings.ctm
